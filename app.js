cccb1223b8759bca72c41569d73590ad157a47ad
Kushal <fizzykushal@gmail.com>
2019-12-21 01:43:59 -0500

    added delete

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require("express");
 app = express();
 var bodyParser = require("body-Parser");
 var mongoose = require("mongoose");
+var expressSantizer = require("express-sanitizer");
+
 var methodOverride = require("method-override");
 
 mongoose.connect("mongodb://localhost/newBlog_app", { useNewUrlParser: true });
@@ -10,6 +12,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(methodOverride("_method"));
+ap.use(expressSantizer());
 
 
 var blogSchema = new mongoose.Schema({
@@ -47,6 +50,7 @@ app.get("/blogs/new", function (req, res) {
 });
 
 app.post("/blogs", function (req, res) {
+    req.body.blog.body = req.sanitize("req.body.blog.body");
     Blog.create(req.body.blog,function (err, newBlog) {
         if(err){
             res.render("new");
@@ -79,6 +83,7 @@ app.get("/blogs/:id/edit", function (req,res) {
 });
 
 app.put("/blogs/:id", function(req,res){
+    req.body.blog.body = req.sanitize("req.body.blog.body");
     Blog.findByIdAndUpdate(req.params.id, req.body.blog, function (err, foundBlog) {
         if(err){
             res.redirect("/blogs");
