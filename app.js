a11a2529af91b90e811056c96b9d2db46130cd4e
Kushal <fizzykushal@gmail.com>
2019-12-21 13:59:26 -0500

    ui improvements

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(methodOverride("_method"));
-ap.use(expressSantizer());
+app.use(expressSantizer());
 
 
 var blogSchema = new mongoose.Schema({
@@ -50,7 +50,7 @@ app.get("/blogs/new", function (req, res) {
 });
 
 app.post("/blogs", function (req, res) {
-    req.body.blog.body = req.sanitize("req.body.blog.body");
+    // req.body.blog.body = req.sanitize("req.body.blog.body");
     Blog.create(req.body.blog,function (err, newBlog) {
         if(err){
             res.render("new");
@@ -83,7 +83,7 @@ app.get("/blogs/:id/edit", function (req,res) {
 });
 
 app.put("/blogs/:id", function(req,res){
-    req.body.blog.body = req.sanitize("req.body.blog.body");
+    // req.body.blog.body = req.sanitize("req.body.blog.body");
     Blog.findByIdAndUpdate(req.params.id, req.body.blog, function (err, foundBlog) {
         if(err){
             res.redirect("/blogs");
